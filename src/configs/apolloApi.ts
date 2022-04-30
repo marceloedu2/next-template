5163028457156744b7c288ac146226649539c041
marceloedu2 <marcelo.edu2@gmail.com>
2022-04-30 18:28:03 -0300

    correçoes do build

--- src/configs/apolloApi.ts
+++ src/configs/apolloApi.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const apolloClient = new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_GRAPHQL_URL}/graphql`,
+  uri: 'http://localhost:1337/graphql', //`${process.env.NEXT_PUBLIC_GRAPHQL_URL}/graphql`,
   cache: new InMemoryCache({
     addTypename: false
   })
