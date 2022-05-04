9fb8454918afbb7a6d6590d3afab568af9e0dfb3
marceloedu2 <marcelo.edu2@gmail.com>
2022-05-03 23:45:07 -0300

    .

--- src/pages/ocorrencias.tsx
+++ src/pages/ocorrencias.tsx
@@ -2,35 +2,6 @@ import ToDoListContainer from '@/containers/ToDoList'
 
 import LayoutDefault from '@/layout/default'
 
-export type TStage = {
-  id: string
-  name: string
-  color: string
-  cards?: TTask[]
-}
-
-export type TTask = {
-  id: string
-  columnId?: string
-  description: string
-  priority: {
-    id: string
-    name: string
-    color: string
-  }
-  stage: {
-    id: string
-  }
-  task_type: {
-    id: string
-    name: string
-  }
-  user: {
-    id: string
-    username: string
-  }
-}
-
 const Occurrences: React.FC = () => {
   return (
     <LayoutDefault>
